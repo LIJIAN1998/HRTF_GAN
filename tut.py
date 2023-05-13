8f657845a762ce22ea0a35b30e390053084d633e
LIJIAN1998 <844299030@qq.com>
2023-05-13 23:29:59 +0100

    sh

--- tut.py
+++ tut.py
@@ -25,19 +25,19 @@ subject_ids= 'first'
 # print("keys of a datapoint: ", p.keys())
 
 domain = 'time'
-side_options = ['left', 'right', 'both', 'both-left', 'both-right', 'none']
-for side in side_options:
-    print("side: ", side)
-    sonicom_ds = SONICOM(base_dir / 'SONICOM',  feature_spec={'hrirs': {'side': side, 'domain': domain}}, 
-            target_spec={'side': {}}, group_spec={'subject': {}})
-    print("length of datset: ", len(sonicom_ds))
-    p = sonicom_ds[0]
-    print("target: ", sonicom_ds[0]['target'])
-    print("group: ", sonicom_ds[0]['group'])
-    print("num row, column angles:", len(sonicom_ds.row_angles), len(sonicom_ds.column_angles))
-    print("row, column angles: ", sonicom_ds.row_angles, sonicom_ds.column_angles)
-    print("radii: ", sonicom_ds.radii)
-    print("---------------------------------------------------------------")
+# side_options = ['left', 'right', 'both', 'both-left', 'both-right']
+# for side in side_options:
+#     print("side: ", side)
+#     sonicom_ds = SONICOM(base_dir / 'SONICOM',  feature_spec={'hrirs': {'side': side, 'domain': domain}}, 
+#             target_spec={'side': {}}, group_spec={'subject': {}})
+#     print("length of datset: ", len(sonicom_ds))
+#     p = sonicom_ds[0]
+#     print("target: ", sonicom_ds[0]['target'])
+#     print("group: ", sonicom_ds[0]['group'])
+#     print("num row, column angles:", len(sonicom_ds.row_angles), len(sonicom_ds.column_angles))
+#     print("row, column angles: ", sonicom_ds.row_angles, sonicom_ds.column_angles)
+#     print("radii: ", sonicom_ds.radii)
+#     print("---------------------------------------------------------------")
 
 # ds = SONICOM(base_dir / 'SONICOM',  feature_spec={'hrirs': {'side': side, 'domain': domain}}, 
 #              target_spec={'side': {}}, group_spec={'subject': {}}, subject_ids='random')
@@ -48,17 +48,18 @@ for side in side_options:
 # print("num row, column angles:", len(ds.row_angles), len(ds.column_angles))
 # print("row, column angles: ", ds.row_angles, ds.column_angles)
 # print("radii: ", ds.radii)
-# sonicom_ds = SONICOM(base_dir / 'SONICOM',  feature_spec={'hrirs': {'side': 'left', 'domain': domain}}, 
-#             target_spec={'side': {}}, group_spec={'subject': {}})
-# sonicom_loader = DataLoader(sonicom_ds, collate_fn=collate_dict_dataset)
-# features, target = next(iter(sonicom_loader))
-# print("data from data loader, shape: ", features.shape)
-# print("target: ", target)
 
-# mask = torch.ones(len(sonicom_ds.row_angles), len(sonicom_ds.column_angles))
-# SHTransform = SphericalHarmonicsTransform(max_degree=10, row_angles=sonicom_ds.row_angles, column_angles=sonicom_ds.column_angles,
-#                                           radii=sonicom_ds.radii, selection_mask=mask, coordinate_system='spherical')
+sonicom_ds = SONICOM(base_dir / 'SONICOM',  feature_spec={'hrirs': {'side': 'left', 'domain': domain}}, 
+            target_spec={'side': {}}, group_spec={'subject': {}})
+sonicom_loader = DataLoader(sonicom_ds, collate_fn=collate_dict_dataset)
+features, target = next(iter(sonicom_loader))
+print("data from data loader, shape: ", features.shape)
+print("target: ", target)
 
+mask = torch.ones(len(sonicom_ds.row_angles), len(sonicom_ds.column_angles), dtype=torch.int32)
+SHTransform = SphericalHarmonicsTransform(max_degree=10, row_angles=sonicom_ds.row_angles, column_angles=sonicom_ds.column_angles,
+                                          radii=sonicom_ds.radii, selection_mask=mask, coordinate_system='spherical')
+print("finished")
 # sphericalHarmonics = SHTransform(features)
 # print("spherical harmonics shape: ", sphericalHarmonics.shape)
 # hrir = SHTransform.inverse(sphericalHarmonics)
