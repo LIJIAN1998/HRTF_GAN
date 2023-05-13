a074454a9c6f0da5214970beb5bbda93fc77e53f
LIJIAN1998 <844299030@qq.com>
2023-05-13 22:38:09 +0100

    10min

--- tut.py
+++ tut.py
@@ -25,7 +25,6 @@ subject_ids= 'first'
 # print("keys of a datapoint: ", p.keys())
 
 domain = 'time'
-side = 'left'
 side_options = ['left', 'right', 'both', 'both-left', 'both-right', 'None']
 for side in side_options:
     sonicom_ds = SONICOM(base_dir / 'SONICOM',  feature_spec={'hrirs': {'side': side, 'domain': domain}}, 
@@ -49,16 +48,16 @@ for side in side_options:
 # print("num row, column angles:", len(ds.row_angles), len(ds.column_angles))
 # print("row, column angles: ", ds.row_angles, ds.column_angles)
 # print("radii: ", ds.radii)
-sonicom_ds = SONICOM(base_dir / 'SONICOM',  feature_spec={'hrirs': {'side': 'left', 'domain': domain}}, 
-            target_spec={'side': {}}, group_spec={'subject': {}})
-sonicom_loader = DataLoader(sonicom_ds, collate_fn=collate_dict_dataset)
-features, target = next(iter(sonicom_loader))
-print("data from data loader, shape: ", features.shape)
-print("target: ", target)
+# sonicom_ds = SONICOM(base_dir / 'SONICOM',  feature_spec={'hrirs': {'side': 'left', 'domain': domain}}, 
+#             target_spec={'side': {}}, group_spec={'subject': {}})
+# sonicom_loader = DataLoader(sonicom_ds, collate_fn=collate_dict_dataset)
+# features, target = next(iter(sonicom_loader))
+# print("data from data loader, shape: ", features.shape)
+# print("target: ", target)
 
-mask = torch.ones(len(sonicom_ds.row_angles), len(sonicom_ds.column_angles))
-SHTransform = SphericalHarmonicsTransform(max_degree=10, row_angles=sonicom_ds.row_angles, column_angles=sonicom_ds.column_angles,
-                                          radii=sonicom_ds.radii, selection_mask=mask, coordinate_system='spherical')
+# mask = torch.ones(len(sonicom_ds.row_angles), len(sonicom_ds.column_angles))
+# SHTransform = SphericalHarmonicsTransform(max_degree=10, row_angles=sonicom_ds.row_angles, column_angles=sonicom_ds.column_angles,
+#                                           radii=sonicom_ds.radii, selection_mask=mask, coordinate_system='spherical')
 
 # sphericalHarmonics = SHTransform(features)
 # print("spherical harmonics shape: ", sphericalHarmonics.shape)
