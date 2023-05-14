83c6754adc36010a0b91f577d799f9bdaf62fdeb
LIJIAN1998 <844299030@qq.com>
2023-05-14 11:12:03 +0100

    shape

--- tut.py
+++ tut.py
@@ -69,11 +69,11 @@ mask = torch.ones((len(sonicom_ds.row_angles), len(sonicom_ds.column_angles), 51
 SHTransform = SphericalHarmonicsTransform(max_degree=10, row_angles=sonicom_ds.row_angles, column_angles=sonicom_ds.column_angles,
                                           radii=sonicom_ds.radii, selection_mask=mask, coordinate_system='spherical')
 
-harmonics_shape, mask_shape, hrir_shape, masked_hrir_shape = SHTransform(features)
+harmonics_shape, mask_shape, hrir_shape = SHTransform(features)
 print("harmonics: ", harmonics_shape)
 print("mask: ", mask_shape)
 print("hrir: ", hrir_shape)
-print("masked hrir: ", masked_hrir_shape)
+# print("masked hrir: ", masked_hrir_shape)
 # sphericalHarmonics = SHTransform(features)
 # print("spherical harmonics shape: ", sphericalHarmonics.shape)
 # print("finished")
