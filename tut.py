fb3ae525d7c70e4464c14edf86586bcc771a4c9a
LIJIAN1998 <844299030@qq.com>
2023-05-17 13:47:29 +0100

    SHT

--- tut.py
+++ tut.py
@@ -69,9 +69,10 @@ print("target: ", target)
 
 mask = torch.zeros((len(sonicom_ds.row_angles), len(sonicom_ds.column_angles), 1), dtype=bool)
 # print("input mask: ", mask)
-SHTransform = SphericalHarmonicsTransform(max_degree=10, row_angles=sonicom_ds.row_angles, column_angles=sonicom_ds.column_angles,
-                                          radii=sonicom_ds.radii, selection_mask=mask, coordinate_system='spherical')
-
+# SHTransform = SphericalHarmonicsTransform(max_degree=10, row_angles=sonicom_ds.row_angles, column_angles=sonicom_ds.column_angles,
+#                                           radii=sonicom_ds.radii, selection_mask=mask, coordinate_system='spherical')
+SHT = SphericalHarmonicsTransform(10, sonicom_ds.row_angles, sonicom_ds.column_angles, sonicom_ds.radii, 
+                                  np.all(np.ma.getmask(sonicom_ds[0]['features']), axis=3), dtype=bool)
 # harmonics_shape, mask_shape, hrir_shape = SHTransform(features)
 # print("harmonics: ", harmonics_shape)
 # print("mask: ", mask_shape)
