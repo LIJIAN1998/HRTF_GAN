48182c83e242a4a8e84e30a55bb7274e337d171d
LIJIAN1998 <844299030@qq.com>
2023-06-06 18:10:49 +0100

    256

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def main(config, mode):
             SHT = SphericalHarmonicsTransform(28, ds.row_angles, ds.column_angles, ds.radii, masks[i].detach().cpu().numpy().astype(bool))
             h = SHT.inverse(hr[i].T.detach().cpu().numpy())
             print(h.shape)
-            h = torch.from_numpy(h.T).reshape(h.size(0), num_radii, num_row_angles, num_col_angles)
+            h = torch.from_numpy(h.T).reshape(256, num_radii, num_row_angles, num_col_angles)
             recon_coef_list.append(h)
         recons = torch.stack(recon_coef_list)
         print("recons:", recons.shape, recons.device.type)
