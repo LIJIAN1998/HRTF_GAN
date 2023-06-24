eb3c0bec26add47bfde3c94fe28dc54b633d7ece
LIJIAN1998 <844299030@qq.com>
2023-06-24 23:40:27 +0100

    test matlab

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import torch
+import subprocess 
+# print("using cuda? ", torch.cuda.is_available())
 
-print("using cuda? ", torch.cuda.is_available())
+matlab_path = '/rds/general/user/jl2622/projects/sonicom/live/matlab/R2021a/bin/matlab'
+
+matlab_script_path = './evaluation/test.m'
+command = [matlab_path, '-batch', f"run('{matlab_script_path}')"]
+subprocess.run(command)
