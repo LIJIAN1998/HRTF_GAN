c6f79bfe32e14046bbca168aebeda93df68fd455
LIJIAN1998 <844299030@qq.com>
2023-06-27 21:18:23 +0100

    test.m

--- evaluation/test.m
+++ evaluation/test.m
@@ -2,3 +2,6 @@ function [result_int, result_str] = multiply_and_concat(num, str)
     result_int = num * 10;
     result_str = ['kunkun', str];
 end
+
+% Call the function with the provided arguments
+multiply_and_concat(input_int, input_str);
