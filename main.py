ff076764cb5d8f9fd29bc311b39d18db46813185
LIJIAN1998 <844299030@qq.com>
2023-07-22 11:31:45 +0100

    no loc eval

--- main.py
+++ main.py
@@ -178,13 +178,13 @@ def main(config, mode):
 
         file_ext = f'lsd_errors_hrtf_selection_minimum_data.pickle'
         run_lsd_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='minimum')
-        file_ext = f'loc_errors_hrtf_selection_minimum_data.pickle'
-        run_localisation_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='minimum')
+        # file_ext = f'loc_errors_hrtf_selection_minimum_data.pickle'
+        # run_localisation_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='minimum')
 
         file_ext = f'lsd_errors_hrtf_selection_maximum_data.pickle'
         run_lsd_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='maximum')
-        file_ext = f'loc_errors_hrtf_selection_maximum_data.pickle'
-        run_localisation_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='maximum')
+        # file_ext = f'loc_errors_hrtf_selection_maximum_data.pickle'
+        # run_localisation_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='maximum')
 
     elif mode == "debug":
         # ds = load_function(data_dir, features_spec={'hrirs': {'samplerate': config.hrir_samplerate, 'side': 'both', 'domain': 'time'}})
