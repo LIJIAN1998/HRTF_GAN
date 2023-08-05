99ff8a86d296977973446b8d824d45248d5457ed
LIJIAN1998 <844299030@qq.com>
2023-08-05 22:51:10 +0100

    float 50

--- main.py
+++ main.py
@@ -227,12 +227,12 @@ def main(config, mode):
         merge = np.ma.concatenate([left, right], axis=3)
         original_mask = np.all(np.ma.getmaskarray(merge), axis=3)
         # print(original_mask)
-        SHT = SphericalHarmonicsTransform(28, left_hrtf.row_angles, left_hrtf.column_angles, left_hrtf.radii, original_mask.astype(bool))
+        SHT = SphericalHarmonicsTransform(50, left_hrtf.row_angles, left_hrtf.column_angles, left_hrtf.radii, original_mask.astype(bool))
         sh_coef = torch.from_numpy(SHT(merge)).float()
         print("coef: ", sh_coef.shape, sh_coef.dtype)
         merge = torch.from_numpy(merge.data) # w x h x r x nbins
         harmonics = torch.from_numpy(SHT.get_harmonics()).float()
-        print("harmonics shape: ", harmonics.shape)
+        print("harmonics shape: ", harmonics.shape, harmonics.dtype)
         inverse = harmonics @ sh_coef
         print("inverse: ", inverse.shape)
         inverse2 = torch.from_numpy(SHT.inverse(sh_coef.numpy()))
