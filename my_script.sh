b2975143dfc681341fcebbfb5156f3319cef2af6
LIJIAN1998 <844299030@qq.com>
2023-08-06 13:03:39 +0100

    test_env

--- my_script.sh
+++ my_script.sh
@@ -2,10 +2,11 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL # required to send email notifcations
 #SBATCH --mail-user=jl2622 # required to send email notifcations - please replace <your_username> with your college login name or email address
-export PATH=/vol/bitbucket/jl2622/hrtf_env/bin/:$PATH
+export PATH=/vol/bitbucket/jl2622/test_env/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
-python main.py debug --hpc False --tag ari-upscale-4
+# python main.py debug --hpc False --tag ari-upscale-4
+python test.py
--- test.py
+++ test.py
@@ -1,8 +1,22 @@
 import argparse
 import torch
 import json
+import torch.nn as nn
 print("test")
 print("using cuda? ", torch.cuda.is_available())
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print("device: ", device)
+x = torch.randn(1, 256, 2116).to(device)
+print(x.shape)
+conv1 = nn.Sequential(
+    nn.Conv1d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
+    nn.BatchNorm1d(256),
+    nn.ReLU(),
+).to(device)
+x = conv1(x)
+print(x.shape)
+print('done')
 
 # def main(index):
 #     with open(f"config_files/config_{index}.json", 'r') as f:
