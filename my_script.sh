1a5bac9edcedc7383dab0294becbc442a42cb67c
LIJIAN1998 <844299030@qq.com>
2023-08-06 14:02:59 +0100

    cuda11.7

--- my_script.sh
+++ my_script.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=jl2622 # required to send email notifcations - please replace <your_username> with your college login name or email address
 export PATH=/vol/bitbucket/jl2622/test_env/bin/:$PATH
 source activate
-source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
+source /vol/cuda/11.7.1/setup.sh
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 nvcc -V
