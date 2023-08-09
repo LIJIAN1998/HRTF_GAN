e7bd0979012688445ef2cce891a294956c1170ac
LIJIAN1998 <844299030@qq.com>
2023-08-09 09:10:45 +0100

    transform load

--- main_2.py
+++ main_2.py
@@ -139,6 +139,8 @@ def main(config, mode):
             train_prefetcher, _ = load_hrtf(config)
         print("transform applied: ", config.transform_flag)
         print("train fetcher: ", len(train_prefetcher))
+        data = next(train_prefetcher)
+        print(data.keys())
         # Trains the model, according to the parameters specified in Config
         # util.initialise_folders(config, overwrite=True)
         # train(config, train_prefetcher)
