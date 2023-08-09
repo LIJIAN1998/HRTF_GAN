886472631bed1eec04bc037069db025380103e18
LIJIAN1998 <844299030@qq.com>
2023-08-09 13:47:27 +0100

    debug sht

--- hrtfdata/transforms/hrirs.py
+++ hrtfdata/transforms/hrirs.py
@@ -338,11 +338,19 @@ class SphericalHarmonicsTransform:
         else:
             # X, Y, Z -> azimuths, elevations, radii
             grid[..., 0], grid[..., 1], grid[..., 2] = cartesian2spherical(grid[..., 0], grid[..., 1], grid[..., 2], out_angles_as_degrees=False)
-        # Convert elevations to zeniths, azimuths, elevations, radii
-        grid[..., 1] = np.pi/2 - grid[..., 1]
+        # # Convert elevations to zeniths, azimuths, elevations, radii
+        # grid[..., 1] = np.pi/2 - grid[..., 1]
+        # Convert elevations and azimuths to be between [0, 2*pi] and [0, pi], respectively.
+        grid[..., 0] = np.pi / 2 + grid[..., 0]  # elevations
+        grid[..., 1] = np.pi + grid[..., 1]  # azimuths
 
         selected_angles = grid[~selection_mask]
-        self._harmonics = np.column_stack([np.real(sph_harm(order, degree, selected_angles[:, 0], selected_angles[:, 1])) for degree in np.arange(max_degree+1) for order in np.arange(-degree, degree+1)])
+        # self._harmonics = np.column_stack([np.real(sph_harm(order, degree, selected_angles[:, 0], selected_angles[:, 1]))
+        #                                     for degree in np.arange(max_degree+1)
+        #                                       for order in np.arange(-degree, degree+1)])
+        self._harmonics = np.column_stack([np.real(sph_harm(order, degree, selected_angles[:, 1], selected_angles[:, 0]))
+                                            for degree in np.arange(max_degree + 1)
+                                              for order in np.arange(-degree, degree + 1)]) 
         self._valid_mask = ~selection_mask
 
 
