5696be4a8ce6f2e55e6b715817dc4182988b8eb4
LIJIAN1998 <844299030@qq.com>
2023-08-11 13:14:14 +0100

    check nan

--- model/test.py
+++ model/test.py
@@ -101,6 +101,7 @@ def test(config, val_prefetcher):
         SHT = SphericalHarmonicsTransform(28, ds.row_angles, ds.column_angles, ds.radii, masks[0].numpy().astype(bool))
         harmonics = torch.from_numpy(SHT.get_harmonics()).float().to(device)
         recon = recon * std + mean
+        print("recon nan? ", torch.isnan(recon).any())
         sr = harmonics @ recon[0].T
         sr = sr.reshape(-1, num_row_angles, num_col_angles, num_radii, nbins)
         margin = 1.8670232e-08
