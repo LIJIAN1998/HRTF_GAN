dc9a2912e73c7176d426c707fda54e3e537548ea
LIJIAN1998 <844299030@qq.com>
2023-08-21 09:25:17 +0100

    import model

--- model/ae.py
+++ model/ae.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from base_blocks import *
+from model.base_blocks import *
 
 class Reshape(nn.Module):
     def __init__(self, *args):
