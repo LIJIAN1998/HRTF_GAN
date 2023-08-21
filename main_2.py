ae1f88eb10fb2c8be64e61c91b6a593dbe43286f
LIJIAN1998 <844299030@qq.com>
2023-08-21 17:18:42 +0100

    -20 20

--- main_2.py
+++ main_2.py
@@ -274,7 +274,7 @@ def main(config, mode):
 
         data = sh_coef.view(-1)
         data_np = data.numpy()
-        plt.hist(data_np, bins=20, range=(-50, 50), edgecolor='black')
+        plt.hist(data_np, bins=20, range=(-20, 20), edgecolor='black')
         plt.xlabel('Value')
         plt.ylabel('Frequency')
         plt.title('Histogram of Tensor Data')
