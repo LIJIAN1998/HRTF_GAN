cc3311d3e31ca8c0908610dada4eec11c67ba368
LIJIAN1998 <844299030@qq.com>
2023-08-29 14:41:52 +0100

    max lsd select

--- main.py
+++ main.py
@@ -215,11 +215,11 @@ def main(config, mode):
 
         file_ext = f'lsd_errors_hrtf_selection_minimum_data.pickle'
         run_lsd_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='minimum')
-        file_ext = f'loc_errors_hrtf_selection_maximum_data.pickle'
-        run_localisation_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='maximum')
+        # file_ext = f'loc_errors_hrtf_selection_minimum_data.pickle'
+        # run_localisation_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='minimum')
 
-        # file_ext = f'lsd_errors_hrtf_selection_maximum_data.pickle'
-        # run_lsd_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='maximum')
+        file_ext = f'lsd_errors_hrtf_selection_maximum_data.pickle'
+        run_lsd_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='maximum')
         # file_ext = f'loc_errors_hrtf_selection_maximum_data.pickle'
         # run_localisation_evaluation(config, config.hrtf_selection_dir, file_ext, hrtf_selection='maximum')
 
