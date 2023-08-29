70d5708bd68ac2f82cebe8461f76ee56e3a9fa1e
LIJIAN1998 <844299030@qq.com>
2023-08-29 19:46:39 +0100

    plot min max selection

--- visualize.py
+++ visualize.py
@@ -59,7 +59,7 @@ def plot_lsd(lsd_2d_1, lsd_2d_2, row_angles, column_angles, filename):
     plt.scatter(x, y, c=values_1, cmap='OrRd', s=50, marker='o', edgecolor='black', vmin=min_value, vmax=max_value)
     plt.colorbar(label='Average LSD Error')
     plt.ylabel('Elevation (degree)')
-    plt.title('AE-GAN')
+    plt.title('Selection-1')
     plt.xticks([])  # Remove x-axis labels and ticks
 
     plt.subplot(2, 1, 2)
@@ -67,7 +67,7 @@ def plot_lsd(lsd_2d_1, lsd_2d_2, row_angles, column_angles, filename):
     plt.colorbar(label='Average LSD Error')
     plt.xlabel('Azimuth (degree)')
     plt.ylabel('Elevation (degree)')
-    plt.title('Barycentric')
+    plt.title('Selection-2')
 
     plt.tight_layout()
 
@@ -95,8 +95,6 @@ degree = int(np.sqrt(num_row_angles*num_col_angles*num_radii/upscale_factor) - 1
 print("domain: ", domain, "upscale factor: ", upscale_factor)
 
 ngpu = config.ngpu
-valid_dir = config.valid_path
-valid_gt_dir = config.valid_gt_path
 
 nbins = config.nbins_hrtf
 if config.merge_flag:
@@ -104,10 +102,10 @@ if config.merge_flag:
 
 device = torch.device(config.device_name if (
     torch.cuda.is_available() and ngpu > 0) else "cpu")
-model = AutoEncoder(nbins=nbins, in_order=degree, latent_dim=config.latent_dim, base_channels=256, num_features=512, out_oder=max_order)
-print("Build VAE model successfully.")
-model.load_state_dict(torch.load(f"{config.model_path}/Gen_216bn.pt", map_location=torch.device('cpu')))
-print(f"Load VAE model weights `{os.path.abspath(config.model_path)}` successfully.")
+# model = AutoEncoder(nbins=nbins, in_order=degree, latent_dim=config.latent_dim, base_channels=256, num_features=512, out_oder=max_order)
+# print("Build VAE model successfully.")
+# model.load_state_dict(torch.load(f"{config.model_path}/Gen_216bn.pt", map_location=torch.device('cpu')))
+# print(f"Load VAE model weights `{os.path.abspath(config.model_path)}` successfully.")
 
 _, test_prefetcher = load_hrtf(config)
 test_prefetcher.reset()
@@ -118,36 +116,48 @@ hrtf = batch_data["hrtf"]
 masks = batch_data["mask"]
 sample_id = batch_data["id"].item()
 
-model.eval()
-with torch.no_grad():
-    recon = model(lr_coefficient)
+# model.eval()
+# with torch.no_grad():
+#     recon = model(lr_coefficient)
 
-original_mask = masks[0].numpy().astype(bool)
-SHT = SphericalHarmonicsTransform(max_order, ds.row_angles, ds.column_angles, ds.radii, original_mask)
-harmonics = torch.from_numpy(SHT.get_harmonics()).float().to(device)
-recon_hrtf = harmonics @ recon[0].T
-ori_hrtf = hrtf[0].reshape(nbins, -1).T
-print("subject: ", sample_id)
+# original_mask = masks[0].numpy().astype(bool)
+# SHT = SphericalHarmonicsTransform(max_order, ds.row_angles, ds.column_angles, ds.radii, original_mask)
+# harmonics = torch.from_numpy(SHT.get_harmonics()).float().to(device)
+# recon_hrtf = harmonics @ recon[0].T
+# ori_hrtf = hrtf[0].reshape(nbins, -1).T
+# print("subject: ", sample_id)
 
-lsd_arr = calc_lsd(ori_hrtf, recon_hrtf, domain='magnitude_db')
-lsd_2d = replace_lsd(lsd_arr, upscale_factor)
-# filename = "lsd.png"
-# plot_lsd(lsd_2d, row_angles, column_angles, filename)
+# lsd_arr = calc_lsd(ori_hrtf, recon_hrtf, domain='magnitude_db')
+# lsd_2d = replace_lsd(lsd_arr, upscale_factor)
 
-# plot for barycentric
+# for barycentric
 file_name = '/' + f"{config.dataset}_{sample_id}.pickle"
 with open(config.valid_mag_path + file_name, "rb") as f:
     hr_hrtf = pickle.load(f).permute(1, 2, 0, 3)  # r x w x h x nbins -> w x h x r x nbins
 
-barycentric_data_folder = f'/barycentric_interpolated_data_{config.upscale_factor}'
-barycentric_output_path = config.barycentric_hrtf_dir + barycentric_data_folder
-with open(barycentric_output_path + file_name, "rb") as f:
-    bary_hrtf = pickle.load(f)   # w x h x r x nbins
+# barycentric_data_folder = f'/barycentric_interpolated_data_{config.upscale_factor}'
+# barycentric_output_path = config.barycentric_hrtf_dir + barycentric_data_folder
+# with open(barycentric_output_path + file_name, "rb") as f:
+#     bary_hrtf = pickle.load(f)   # w x h x r x nbins
 
 hr_hrtf = hr_hrtf.reshape(-1, nbins)
-bary_hrtf = bary_hrtf.reshape(-1, nbins)
-lsd_arr_bary = calc_lsd(hr_hrtf, bary_hrtf, domain="magnitude")
-lsd_2d_bary = replace_lsd(lsd_arr_bary, upscale_factor)
-filename = f"lsd_{upscale_factor}.png"
-plot_lsd(lsd_2d, lsd_2d_bary, row_angles, column_angles, filename)
+# bary_hrtf = bary_hrtf.reshape(-1, nbins)
+# lsd_arr_bary = calc_lsd(hr_hrtf, bary_hrtf, domain="magnitude")
+# lsd_2d_bary = replace_lsd(lsd_arr_bary, upscale_factor)
+# filename = f"lsd_{upscale_factor}.png"
+# plot_lsd(lsd_2d, lsd_2d_bary, row_angles, column_angles, filename)
 
+with open(config.hrtf_selection_dir + "maximum.pickle", "rb") as f:
+    max_hrtf = pickle.load(f)
+
+lsd_max_select = calc_lsd(hr_hrtf, max_hrtf, domain="magnitude")
+lsd_max_2d = lsd_max_select.reshape(72,12)
+
+with open(config.hrtf_selection_dir + "minimum.pickle", "rb") as f:
+    min_hrtf = pickle.load(f)
+
+lsd_min_select = calc_lsd(hr_hrtf, min_hrtf, domain="magnitude")
+lsd_min_2d = lsd_min_select.reshape(72,12)
+
+filename = "lsd_select.png"
+plot_lsd(lsd_min_2d, lsd_max_2d, row_angles, column_angles, filename)
